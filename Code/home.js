925dbe91e94a49420a6ab935bda6b5f6b0ae8cc8
ashok686 <ashok.s@appinessworld.com>
2022-05-16 00:09:35 +0530

    final project

---
+++ Code/home.js
@@ -0,0 +1,18 @@
+let fval = document.querySelector('#qty-1')
+let rval = document.querySelector('#val1')
+let total = document.querySelector('#asdf')
+let checkOutTotal = document.querySelector('#total-checkout')
+fval.addEventListener('change', () => {
+    let val = fval.value
+    rval.innerText = val * 499
+    total.innerText = rval.innerText
+    checkOutTotal.innerText = rval.innerText
+})
+
+let remove = document.querySelector('#remove')
+let td = document.querySelector('#singleItem')
+
+remove.addEventListener('click', () => {
+    td.remove();
+
+})
